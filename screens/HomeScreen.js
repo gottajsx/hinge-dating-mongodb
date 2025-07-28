dcb9c999914b1320f94ef3b63a2bb3b748a2d8ae
gottajsx <gottajsx@gmail.com>
2025-07-28 13:02:43 +0200

    Add HomeScreen stub

---
+++ screens/HomeScreen.js
@@ -0,0 +1,29 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Pressable,
+  Image,
+} from 'react-native';
+import React, {useEffect, useState, useCallback} from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import 'core-js/stable/atob';
+import Entypo from 'react-native-vector-icons/Entypo';
+import Octicons from 'react-native-vector-icons/Octicons';
+import Feather from 'react-native-vector-icons/Feather';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useFocusEffect, useNavigation} from '@react-navigation/native';
+import {jwtDecode} from 'jwt-decode';
+import axios from 'axios';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello Monde !</Text>
+    </View>
+  );
+}
