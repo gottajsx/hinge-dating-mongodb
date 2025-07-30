a19cd6959ae4aad6ce9c3755f30cbd4f16c279ee
gottajsx <gottajsx@gmail.com>
2025-07-30 15:20:15 +0200

    Add EmailScreen

--- screens/EmailScreen.js
+++ screens/EmailScreen.js
@@ -2,28 +2,121 @@ import {
   StyleSheet,
   Text,
   View,
-  ScrollView,
-  Pressable,
+  SafeAreaView,
+  Platform,
+  TouchableOpacity,
   Image,
+  TextInput,
 } from 'react-native';
-import React, {useEffect, useState, useCallback} from 'react';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import 'core-js/stable/atob';
-import Entypo from 'react-native-vector-icons/Entypo';
-import Octicons from 'react-native-vector-icons/Octicons';
-import Feather from 'react-native-vector-icons/Feather';
-import AntDesign from 'react-native-vector-icons/AntDesign';
-import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {useFocusEffect, useNavigation} from '@react-navigation/native';
-import {jwtDecode} from 'jwt-decode';
-import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+//import Fontisto from '@react-native-vector-icons/fontisto';
+//import Ionicons from '@react-native-vector-icons/ionicons';
+import { Fontisto } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import {useNavigation} from '@react-navigation/native';
+import {
+  getRegistrationProgress,
+  saveRegistrationProgress,
+} from '../utils/registrationUtils';
 
-export default function EmailScreen() {
+const EmailScreen = () => {
+  const [email, setEmail] = useState('');
+  const navigation = useNavigation();
+  useEffect(() => {
+    getRegistrationProgress('Email').then(progressData => {
+      if (progressData) {
+        setEmail(progressData.email || '');
+      }
+    });
+  }, []);
+  const handleNext = () => {
+    if (email.trim() !== '') {
+      saveRegistrationProgress('Email', {email});
+    }
+    navigation.navigate('Password', {
+      email: email,
+    });
+  };
   return (
-    <View>
-      <Text>Hello Monde !</Text>
-    </View>
+    <SafeAreaView
+      style={{
+        paddingTop: Platform.OS === 'android' ? 35 : 0,
+        flex: 1,
+        backgroundColor: 'white',
+      }}>
+      <View style={{marginTop: 80, marginHorizontal: 20}}>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <View
+            style={{
+              width: 44,
+              height: 44,
+              borderRadius: 22,
+              borderWidth: 2,
+              borderColor: 'black',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Fontisto name="email" size={26} color="black" />
+          </View>
+          <Image
+            style={{width: 100, height: 40}}
+            source={{
+              uri: 'https://cdn-icons-png.flaticon.com/128/10613/10613685.png',
+            }}
+          />
+        </View>
+
+        <Text
+          style={{
+            fontSize: 25,
+            fontWeight: 'bold',
+            fontFamily: 'GeezaPro-Bold',
+            marginTop: 15,
+          }}>
+          Provide your email
+        </Text>
+
+        <Text style={{marginTop: 10, fontSize: 15, color: 'gray'}}>
+          Email Verification helps us keep the account secure
+        </Text>
+
+        <TextInput
+          value={email}
+          onChangeText={text => setEmail(text)}
+          autoFocus={true}
+          placeholder="Enter your email"
+          placeholderTextColor={'#BEBEBE'}
+          style={{
+            width: 340,
+            marginVertical: 10,
+            marginTop: 25,
+            borderBottomColor: 'black',
+            borderBottomWidth: 1,
+            paddingBottom: 10,
+            fontFamily: 'GeezaPro-Bold',
+            fontSize: email ? 22 : 22,
+          }}
+        />
+
+        <Text style={{color: 'gray', marginTop: 7, fontSize: 15}}>
+          Note: You will be asked to verify your email
+        </Text>
+
+        <TouchableOpacity
+          onPress={handleNext}
+          activeOpacity={0.8}
+          style={{marginTop: 30, marginLeft: 'auto'}}>
+          <Ionicons
+            name="chevron-forward-circle-outline"
+            size={45}
+            color="#581845"
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
-}
+};
+
+export default EmailScreen;
+
+const styles = StyleSheet.create({});
