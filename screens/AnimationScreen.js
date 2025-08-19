7a8278daa4864439681499ea4e48d7a3d233d78f
gottajsx <gottajsx@gmail.com>
2025-08-19 13:59:39 +0200

    Add AnimationScreen

--- screens/AnimationScreen.js
+++ screens/AnimationScreen.js
@@ -1,29 +1,37 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  Pressable,
-  Image,
-} from 'react-native';
-import React, {useEffect, useState, useCallback} from 'react';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import 'core-js/stable/atob';
-import Entypo from 'react-native-vector-icons/Entypo';
-import Octicons from 'react-native-vector-icons/Octicons';
-import Feather from 'react-native-vector-icons/Feather';
-import AntDesign from 'react-native-vector-icons/AntDesign';
-import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {useFocusEffect, useNavigation} from '@react-navigation/native';
-import {jwtDecode} from 'jwt-decode';
-import axios from 'axios';
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import LottieView from 'lottie-react-native';
 
-export default function AnimationScreen() {
+
+const AnimationScreen = () => {
+  const navigation = useNavigation();
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.goBack();
+    }, 1000);
+  }, []);
   return (
-    <View>
-      <Text>Hello Monde !</Text>
+    <View style={{flex:1,backgroundColor:"White",justifyContent:"center",alignItems:"center"}}>
+       <View>
+        <LottieView
+          source={require('../assets/cross.json')}
+          style={{
+            height: 260,
+            width: 300,
+            alignSelf: 'center',
+            marginTop: 40,
+            justifyContent: 'center',
+          }}
+          autoPlay
+          loop={true}
+          speed={0.7}
+        />
+      </View>
     </View>
   );
-}
+};
+
+export default AnimationScreen;
+
+const styles = StyleSheet.create({});
