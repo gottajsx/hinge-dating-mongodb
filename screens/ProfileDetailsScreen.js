ea5af2d853985a207e21078c828473dc812ed666
gottajsx <gottajsx@gmail.com>
2025-08-21 12:48:29 +0200

    Add ProfileDetailsScreen screen

--- screens/ProfileDetailsScreen.js
+++ screens/ProfileDetailsScreen.js
@@ -1,29 +1,366 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  Pressable,
-  Image,
+import { 
+  ScrollView, 
+  StyleSheet, 
+  Text, 
+  View, 
+  Image 
 } from 'react-native';
-import React, {useEffect, useState, useCallback} from 'react';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import 'core-js/stable/atob';
-import Entypo from 'react-native-vector-icons/Entypo';
-import Octicons from 'react-native-vector-icons/Octicons';
-import Feather from 'react-native-vector-icons/Feather';
-import AntDesign from 'react-native-vector-icons/AntDesign';
-import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import React from 'react';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import { Ionicons, Entypo, AntDesign } from '@expo/vector-icons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {useFocusEffect, useNavigation} from '@react-navigation/native';
-import {jwtDecode} from 'jwt-decode';
-import axios from 'axios';
 
-export default function ProfileDetailsScreen() {
+
+const ProfileDetailsScreen = () => {
+    const route = useRoute();
+    console.log(route?.params)
   return (
-    <View>
-      <Text>Hello Monde !</Text>
+    <View style={{marginTop:55}}>
+      <ScrollView>
+        <View style={{marginHorizontal: 12, marginVertical: 12}}>
+          {/* {profiles?.map((item, index) => ( */}
+          <>
+            <View>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                }}>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    gap: 10,
+                  }}>
+                  <Text style={{fontSize: 22, fontWeight: 'bold'}}>
+                    {route?.params?.currentProfile?.firstName}
+                  </Text>
+                  <View
+                    style={{
+                      backgroundColor: '#452c63',
+                      paddingHorizontal: 12,
+                      paddingVertical: 4,
+                      borderRadius: 20,
+                    }}>
+                    <Text style={{textAlign: 'center', color: 'white'}}>
+                      new here
+                    </Text>
+                  </View>
+                </View>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    gap: 15,
+                  }}>
+                  <Entypo
+                    name="dots-three-horizontal"
+                    size={22}
+                    color="black"
+                  />
+                </View>
+              </View>
+
+              <View style={{marginVertical: 15}}>
+                <View>
+                  {route?.params?.currentProfile?.imageUrls.length > 0 && (
+                    <View>
+                      <Image
+                        style={{
+                          width: '100%',
+                          height: 350,
+                          resizeMode: 'cover',
+                          borderRadius: 10,
+                        }}
+                        source={{
+                          uri: route?.params?.currentProfile?.imageUrls[0]
+                        }}
+                      />
+                      <View
+                        style={{
+                          position: 'absolute',
+                          bottom: 10,
+                          right: 10,
+                          backgroundColor: 'white',
+                          width: 42,
+                          height: 42,
+                          borderRadius: 21,
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                        }}>
+                        <AntDesign name="hearto" size={25} color="#C5B358" />
+                      </View>
+                    </View>
+                  )}
+                </View>
+
+                <View style={{marginVertical: 15}}>
+                  {route?.params?.currentProfile?.prompts.slice(0, 1).map(prompt => (
+                    <>
+                      <View
+                        key={prompt.id}
+                        style={{
+                          backgroundColor: 'white',
+                          padding: 12,
+                          borderRadius: 10,
+                          height: 150,
+                          justifyContent: 'center',
+                        }}>
+                        <Text style={{fontSize: 15, fontWeight: '500'}}>
+                          {prompt.question}
+                        </Text>
+                        <Text
+                          style={{
+                            fontSize: 20,
+                            fontWeight: '600',
+                            marginTop: 20,
+                          }}>
+                          {prompt.answer}
+                        </Text>
+                      </View>
+                      <View
+                        style={{
+                          position: 'absolute',
+                          bottom: 10,
+                          right: 10,
+                          backgroundColor: 'white',
+                          width: 42,
+                          height: 42,
+                          borderRadius: 21,
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                          shadowColor: '#000',
+                          shadowOffset: {width: 0, height: 1},
+                          shadowOpacity: 0.25,
+                          shadowRadius: 3.84,
+                          // Shadow properties for Android
+                          elevation: 5,
+                        }}>
+                        <AntDesign name="hearto" size={25} color="#C5B358" />
+                      </View>
+                    </>
+                  ))}
+                </View>
+
+                {/* profile details to come here */}
+
+                <View>
+                  {route?.params?.currentProfile?.imageUrls?.slice(1, 3).map((item, index) => (
+                    <View key={index} style={{marginVertical: 10}}>
+                      <Image
+                        style={{
+                          width: '100%',
+                          height: 350,
+                          resizeMode: 'cover',
+                          borderRadius: 10,
+                        }}
+                        source={{
+                          uri: item
+                        }}
+                      />
+
+                      <View
+                        style={{
+                          position: 'absolute',
+                          bottom: 10,
+                          right: 10,
+                          backgroundColor: 'white',
+                          width: 42,
+                          height: 42,
+                          borderRadius: 21,
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                        }}>
+                        <AntDesign name="hearto" size={25} color="#C5B358" />
+                      </View>
+                    </View>
+                  ))}
+                </View>
+
+                <View style={{marginVertical: 15}}>
+                  {route?.params?.currentProfile?.prompts.slice(1, 2).map(prompt => (
+                    <>
+                      <View
+                        key={prompt.id}
+                        style={{
+                          backgroundColor: 'white',
+                          padding: 12,
+                          borderRadius: 10,
+                          height: 150,
+                          justifyContent: 'center',
+                        }}>
+                        <Text style={{fontSize: 15, fontWeight: '500'}}>
+                        {prompt.question}
+                        </Text>
+                        <Text
+                          style={{
+                            fontSize: 20,
+                            fontWeight: '600',
+                            marginTop: 20,
+                          }}>
+                          {prompt.answer}
+                        </Text>
+                      </View>
+                      <View
+                        style={{
+                          position: 'absolute',
+                          bottom: 10,
+                          right: 10,
+                          backgroundColor: 'white',
+                          width: 42,
+                          height: 42,
+                          borderRadius: 21,
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                          shadowColor: '#000',
+                          shadowOffset: {width: 0, height: 1},
+                          shadowOpacity: 0.25,
+                          shadowRadius: 3.84,
+                          // Shadow properties for Android
+                          elevation: 5,
+                        }}>
+                        <AntDesign name="hearto" size={25} color="#C5B358" />
+                      </View>
+                    </>
+                  ))}
+                </View>
+
+                <View>
+                  {route?.params?.currentProfile?.imageUrls?.slice(3, 4).map((item, index) => (
+                    <View key={index} style={{marginVertical: 10}}>
+                      <Image
+                        style={{
+                          width: '100%',
+                          height: 350,
+                          resizeMode: 'cover',
+                          borderRadius: 10,
+                        }}
+                        source={{
+                          uri: item
+                        }}
+                      />
+                      <View
+                        style={{
+                          position: 'absolute',
+                          bottom: 10,
+                          right: 10,
+                          backgroundColor: 'white',
+                          width: 42,
+                          height: 42,
+                          borderRadius: 21,
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                        }}>
+                        <AntDesign name="hearto" size={25} color="#C5B358" />
+                      </View>
+                    </View>
+                  ))}
+                </View>
+                <View style={{marginVertical: 15}}>
+                  {route?.params?.currentProfile?.prompts.slice(2, 3).map(prompt => (
+                    <>
+                      <View
+                        key={prompt.id}
+                        style={{
+                          backgroundColor: 'white',
+                          padding: 12,
+                          borderRadius: 10,
+                          height: 150,
+                          justifyContent: 'center',
+                        }}>
+                        <Text style={{fontSize: 15, fontWeight: '500'}}>
+                        {prompt.question}
+                        </Text>
+                        <Text
+                          style={{
+                            fontSize: 20,
+                            fontWeight: '600',
+                            marginTop: 20,
+                          }}>
+                          {prompt.answer}
+                        </Text>
+                      </View>
+                      <View
+                        style={{
+                          position: 'absolute',
+                          bottom: 10,
+                          right: 10,
+                          backgroundColor: 'white',
+                          width: 42,
+                          height: 42,
+                          borderRadius: 21,
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                          shadowColor: '#000',
+                          shadowOffset: {width: 0, height: 1},
+                          shadowOpacity: 0.25,
+                          shadowRadius: 3.84,
+                          // Shadow properties for Android
+                          elevation: 5,
+                        }}>
+                        <AntDesign name="hearto" size={25} color="#C5B358" />
+                      </View>
+                    </>
+                  ))}
+                </View>
+
+                <View>
+                  {route?.params?.currentProfile?.imageUrls?.slice(4, 7).map((item, index) => (
+                    <View key={index} style={{marginVertical: 10}}>
+                      <Image
+                        style={{
+                          width: '100%',
+                          height: 350,
+                          resizeMode: 'cover',
+                          borderRadius: 10,
+                        }}
+                        source={{
+                          uri: item
+                        }}
+                      />
+                      <View
+                        style={{
+                          position: 'absolute',
+                          bottom: 10,
+                          right: 10,
+                          backgroundColor: 'white',
+                          width: 42,
+                          height: 42,
+                          borderRadius: 21,
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                        }}>
+                        <AntDesign name="hearto" size={25} color="#C5B358" />
+                      </View>
+                    </View>
+                  ))}
+                </View>
+              </View>
+
+              {/* <View
+              style={{
+                position:"absolute",
+                bottom: 10,
+                left: 10,
+                backgroundColor: 'white',
+                width: 42,
+                height: 42,
+                borderRadius: 21,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <Entypo name="cross" size={25} color="#C5B358" />
+            </View> */}
+            </View>
+          </>
+          {/* ))} */}
+        </View>
+      </ScrollView>
     </View>
-  );
+  )
 }
+
+export default ProfileDetailsScreen
+
+const styles = StyleSheet.create({})
