8dfbfb6265a34d475af7ecfe0bda8bcd322cb393
gottajsx <gottajsx@gmail.com>
2025-09-06 18:28:11 +0200

    Fix mongodb access

--- api/config/db.js
+++ api/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://sujan:sujan@cluster0.8gyy8sg.mongodb.net/');
+    await mongoose.connect('mongodb+srv://stephanelarance75:saqOLQxHuCJmM5al@cluster0.aihxbwv.mongodb.net/hingeDating?retryWrites=true&w=majority&appName=Cluster0');
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB', error);
